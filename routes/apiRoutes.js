02fd964e84851ae127115ab87cd1564c3e8b7b36
Meghedi Hacopi <hacopian.m@gmail.com>
2020-04-04 14:08:28 -0700

    create user post and user.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,8 +17,17 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
+    });
+  });
+
+  // Create a new user api/newuser
+  app.post("/api/newuser", function(req, res) {
+    db.User.create(req.body).then(function(data) {
+      res.json(data);
     });
   });
 };
